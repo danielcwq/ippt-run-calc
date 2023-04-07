4b4a3d1cfbd5d01648fe8efc4a65c5a6e36e5193
Daniel Ching <dancwq2@gmail.com>
2023-04-07 08:27:53 +0800

    Added more specific instructions

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,8 @@ export default function Home() {
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-            <h1 className="text-2xl font-bold mb-4">2.4km Run Time Tracker</h1>
+            <h1 className="text-2xl font-bold mb-4">2.4km Run Time Calculator</h1>
+            <h2 className="text-xl font-bold mb-4">Enter your target 2.4km run time:</h2>
             <form onSubmit={handleSubmit}>
               <div className="mb-5">
                 <label htmlFor="minutes" className="block mb-2">Minutes:</label>
