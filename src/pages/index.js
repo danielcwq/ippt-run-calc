9b56b9cc2d1c6fdc3d4fb79e9caad84772e44650
Daniel Ching <dancwq2@gmail.com>
2023-04-16 17:41:10 +0800

    Trialing vercel analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -61,14 +61,16 @@ export default function Home() {
 
   return (
     <>
-    <Analaytics/>
+    
       <Head>
         <title>2.4km Run Time Tracker</title>
         <meta name="description" content="2.4km Run Time Tracker App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+      <body>
+        <main className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <Analaytics />
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
@@ -158,6 +160,8 @@ export default function Home() {
           </div>
         </div>
       </main>
+      </body>
+      
     </>
   );
 }
