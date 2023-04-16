d419429f854f0c270c435d6ac31bebf093ed65c3
Daniel Ching <dancwq2@gmail.com>
2023-04-16 17:42:07 +0800

    Revert to original

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { useState } from "react";
-import {Analaytics} from '@vercel/analytics/react'
+
 
 export default function Home() {
   const [minutes, setMinutes] = useState("");
@@ -70,7 +70,6 @@ export default function Home() {
       </Head>
       <body>
         <main className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
-        <Analaytics />
         <div className="relative py-3 sm:max-w-xl sm:mx-auto">
           <div className="absolute inset-0 bg-gradient-to-r from-blue-400 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
           <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
