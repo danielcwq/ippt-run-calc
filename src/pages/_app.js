417be68d0fe19f918b2736800353153f43823439
Daniel Ching <dancwq2@gmail.com>
2023-04-21 14:22:01 +0800

    oops

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,19 +5,8 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <div className="container">
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'GA_MEASUREMENT_ID');
-        `}
-      </Script>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} />
     </div>
   )
 }
