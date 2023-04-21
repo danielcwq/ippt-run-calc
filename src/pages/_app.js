f217051e43641f5b37d5a646669afa2efd3ca26a
Daniel Ching <dancwq2@gmail.com>
2023-04-21 14:19:58 +0800

    Trialing new google tag changes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,23 @@
 import '@/styles/globals.css'
 import { GoogleAnalytics } from 'nextjs-google-analytics'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }) {
-  return(
-    <>
-    <GoogleAnalytics trackPageViews/>
-    <Component {...pageProps} />
-    </>
-  ) 
+  return (
+    <div className="container">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'GA_MEASUREMENT_ID');
+        `}
+      </Script>
+    </div>
+  )
 }
