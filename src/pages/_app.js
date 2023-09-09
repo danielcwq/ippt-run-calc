1b44ae3dacb60b19d9e3e87e44fae7f71d03e4e9
Daniel Ching <dancwq2@gmail.com>
2023-09-09 19:07:57 +0800

    took away useEffect

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,10 @@ import ReactGA from "react-ga4";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.initialize("G-DZRB50TH1X");
-    // To Report Page View
-    ReactGA.send({ hitType: "pageview", page: "/" });
-  }, []);
+  ReactGA.initialize("G-DZRB50TH1X");
+  // To Report Page View
+  ReactGA.send({ hitType: "pageview", page: "/" });
+
   return (
     <div className="container">
       <GoogleAnalytics trackPageViews />
