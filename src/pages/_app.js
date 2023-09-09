caf7b2c8764ff0914b2e9739265f084b306779d5
Daniel Ching <dancwq2@gmail.com>
2023-09-09 19:03:56 +0800

    Added tagging

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
-import '@/styles/globals.css'
-import { GoogleAnalytics } from 'nextjs-google-analytics'
-import Script from 'next/script'
+import "@/styles/globals.css";
+import { GoogleAnalytics } from "nextjs-google-analytics";
+import ReactGA from "react-ga4";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize("G-DZRB50TH1X");
+    // To Report Page View
+    ReactGA.send({ hitType: "pageview", page: "/" });
+  }, []);
   return (
     <div className="container">
       <GoogleAnalytics trackPageViews />
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
